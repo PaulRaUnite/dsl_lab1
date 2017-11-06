5c16611b610e0b190a2fe5238028b4f2f8f6a526
PaulRa <16649084+PaulRaUnite@users.noreply.github.com>
2017-11-06 23:29:45 +0200

    Better typing, encapsulation and docs.

--- ast/tree.py
+++ ast/tree.py
@@ -2,44 +2,47 @@ __all__ = ["Node", "AST", "Value", "Concatenation", "Decision", "Clini"]
 
 
 class Node:
-    """Base class for all nodes of AST."""
-    def __init__(self, value: str, left, right):
-        self.__value: str = value
-        self._lchild: Node = left
-        self._rchild: Node = right
+    """Base class for all nodes of regexp AST."""
+
+    def __init__(self, value: chr, left: 'Node', right: 'Node'):
+        self.__value: chr = value
+        self.__lchild: Node = left
+        self.__rchild: Node = right
 
     def children(self) -> tuple:
-        if self._lchild is not None:
-            if self._rchild is not None:
-                return self._lchild, self._rchild
+        if self.__lchild is not None:
+            if self.__rchild is not None:
+                return self.__lchild, self.__rchild
             else:
-                return self._lchild,
+                return self.__lchild,
         return ()
 
-    def value(self) -> str:
+    def value(self) -> chr:
         return self.__value
 
-    def __str__(self):
+    def __str__(self) -> str:
         pass
 
 
 class AST:
+    """AST(abstract syntax tree) is a structure to represent the regular expressions."""
+
     def __init__(self, root: Node):
         self.__root: Node = root
 
     def root(self) -> Node:
         return self.__root
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.__root.__str__()
 
 
 class Value(Node):
-    """Value is a node that represents leaves."""
-    def __init__(self, s: str):
+    """Value is a node that represents leaves(actual symbols)."""
+    def __init__(self, s: chr):
         super().__init__(s, None, None)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.value()
 
 
@@ -48,8 +51,8 @@ class Concatenation(Node):
     def __init__(self, left: Node, right: Node):
         super().__init__('+', left, right)
 
-    def __str__(self):
-        return "{}+{}".format(self._lchild.__str__(), self._rchild.__str__())
+    def __str__(self) -> str:
+        return "{}+{}".format(self.__lchild.__str__(), self.__rchild.__str__())
 
 
 class Decision(Node):
@@ -57,8 +60,8 @@ class Decision(Node):
     def __init__(self, left: Node, right: Node):
         super().__init__('|', left, right)
 
-    def __str__(self):
-        return "({}|{})".format(self._lchild.__str__(), self._rchild.__str__())
+    def __str__(self) -> str:
+        return "({}|{})".format(self.__lchild.__str__(), self.__rchild.__str__())
 
 
 class Clini(Node):
@@ -66,5 +69,5 @@ class Clini(Node):
     def __init__(self, child: Node):
         super().__init__('*', child, None)
 
-    def __str__(self):
-        return "({})*".format(self._lchild.__str__())
+    def __str__(self) -> str:
+        return "({})*".format(self.__lchild.__str__())
